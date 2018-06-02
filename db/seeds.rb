3f2d0ab9ba6b2618a1c54f62487476818a67e6cb
Antoine STAUMONT <staumont.antoine@me.com>
2018-06-02 17:28:00 +0200

    mise en place seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,72 +8,72 @@
 Conversation.destroy_all
 Conversationcommit.destroy_all
 
-#puts "***--- ADMIN #1 CREATION ---***"
-#user = User.create!(
-#    email: "staumont.antoine@me.com",
-#    password: "123456",
+puts "***--- ADMIN #1 CREATION ---***"
+user = User.create!(
+    email: "staumont.antoine@me.com",
+    password: "123456",
 
 
-#)
-#  prof = Profile.create!(
-#    first_name: "Antoine",
-#    last_name: "STAUMONT",
-#    job: "Chirurgien dentiste",
-#    phone_number: "0660954702",
-#    birthday: "07/04/1977",
-#    city: "Hardelot",
-#    pseudo: "Bossanov",
-#    statut: "yes",
-#    admin: "yes",
-#    biography: "Chirugien dentiste reconverti en développeur web depusi Décembre 2017",
+)
+  prof = Profile.create!(
+    first_name: "Antoine",
+    last_name: "STAUMONT",
+    job: "Chirurgien dentiste",
+    phone_number: "0660954702",
+    birthday: "07/04/1977",
+    city: "Hardelot",
+    pseudo: "Bossanov",
+    statut: "yes",
+    admin: "yes",
+    biography: "Chirugien dentiste reconverti en développeur web depusi Décembre 2017",
 
-#    user_id: user.id,
-#  )
+    user_id: user.id,
+  )
 
-#puts "***--- ADMIN #2 CREATION ---***"
-#user = User.create!(
-#    email: "l.ranchere@orange.fr",
-#    password: "123456",
-
-
-#)
-#  prof = Profile.create!(
-#    first_name: "Loic",
-#    last_name: "RANCHERE",
-#    job: "Chirurgien dentiste",
-#    phone_number: "0670802305",
-#    birthday: "07/04/1962",
-#    city: "Sauzé-Vaussais",
-#    pseudo: "Loic",
-#    statut: "yes",
-#    admin: "yes",
-#    biography: "A remplir",
-
-#    user_id: user.id,
-#  )
+puts "***--- ADMIN #2 CREATION ---***"
+user = User.create!(
+    email: "l.ranchere@orange.fr",
+    password: "123456",
 
 
-#puts "***--- ADMIN #3 CREATION ---***"
-#user = User.create!(
-#    email: "caromeaux@yahoo.fr",
-#    password: "123456",
+)
+  prof = Profile.create!(
+    first_name: "Loic",
+    last_name: "RANCHERE",
+    job: "Chirurgien dentiste",
+    phone_number: "0670802305",
+    birthday: "07/04/1962",
+    city: "Sauzé-Vaussais",
+    pseudo: "Loic",
+    statut: "yes",
+    admin: "yes",
+    biography: "A remplir",
+
+    user_id: user.id,
+  )
 
 
-#)
-#  prof = Profile.create!(
-#    first_name: "Caroline",
-#    last_name: "MEAUX",
-#    job: "Chirurgien dentiste",
-#    phone_number: "0670802305",
-#    birthday: "08/04/1979",
-#    city: "Laventie",
-#    pseudo: "Caroline",
-#    statut: "yes",
-#    admin: "yes",
-#    biography: "A remplir STP",
+puts "***--- ADMIN #3 CREATION ---***"
+user = User.create!(
+    email: "caromeaux@yahoo.fr",
+    password: "123456",
 
- #   user_id: user.id,
- # )
+
+)
+  prof = Profile.create!(
+    first_name: "Caroline",
+    last_name: "MEAUX",
+    job: "Chirurgien dentiste",
+    phone_number: "0670802305",
+    birthday: "08/04/1977",
+    city: "Laventie",
+    pseudo: "Caroline",
+    statut: "yes",
+    admin: "yes",
+    biography: "A remplir STP",
+
+    user_id: user.id,
+  )
 
 
 
