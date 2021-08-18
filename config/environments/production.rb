4c727faf72c7fd4747840192b1b64ee75e9d7e61
antoine staumont <staumont.antoine@me.com>
2021-08-18 17:00:16 +0100

    bug fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ config.webpacker.check_yarn_integrity = false
   # ...
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "www.medentraid.com" }
+  config.action_mailer.default_url_options = { host: "https://entraid.herokuapp.com" }
   # or your custom domain name eg. "www.yourdomain.com"
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
