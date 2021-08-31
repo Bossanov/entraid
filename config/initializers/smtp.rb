f71f0118fbda230dae9ab7f5ad9df7083dfb08a5
antoine staumont <staumont.antoine@me.com>
2021-08-31 09:41:19 +0200

    push pour changement mdp google

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port: 587,
   domain: 'gmail.com',
   user_name: 'antoine.staumont@gmail.com',
-  password: 'Boss62152',
+  password: 'Boss62152?',
   authentication: :login,
   enable_starttls_auto: true
 }
